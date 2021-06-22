64d209f920834d495d9c50ab124dbe952c065608
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2021-06-23 00:54:11 +0530

    Add OAuth2 Authorization support

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # openapi
 Spring boot openapi
 
-Sample project to integrate sprngfox 3.x with spring boot to publish openapi documentation for REST APIs
+Sample project to integrate springfox 3.x with spring boot to publish openapi documentation for REST APIs
+
+Ref: https://javamana.com/2020/11/20201119113453790p.html
--- src/main/java/com/sivaone/openapi/OpenapiApplication.java
+++ src/main/java/com/sivaone/openapi/OpenapiApplication.java
@@ -8,7 +8,9 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.HttpAuthenticationScheme;
+import springfox.documentation.service.OAuth2Scheme;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
@@ -29,6 +31,7 @@ public class OpenapiApplication {
             .title("Product API")
             .description("A CRUD API to demonstrate Springfox 3 integration")
             .version("1.0.0")
+            .contact(new Contact("Siva One", "https://github.com/sivaone", "siva@sivaone.com"))
             .license("MIT")
             .licenseUrl("https://opensource.org/licenses/MIT")
             .build())
@@ -37,11 +40,19 @@ public class OpenapiApplication {
         .paths(PathSelectors.any())
         .build()
         .securityContexts(List.of(securityContext()))
-        .securitySchemes(List.of(securityScheme()));
+        .securitySchemes(List.of(jwtSecurityScheme(), oAuth2SecurityScheme()));
   }
 
-  private SecurityScheme securityScheme() {
+  private SecurityScheme jwtSecurityScheme() {
     return HttpAuthenticationScheme.JWT_BEARER_BUILDER.name("JWT").build();
+  }
+
+  private SecurityScheme oAuth2SecurityScheme() {
+    return OAuth2Scheme.OAUTH2_CLIENT_CREDENTIALS_FLOW_BUILDER
+        .name("oauth2")
+        .tokenUrl("http://localhost:8081/token")
+        .scopes(List.of(new AuthorizationScope("allowAll", "Allow All")))
+        .build();
   }
 
   private SecurityContext securityContext() {
@@ -52,7 +63,7 @@ public class OpenapiApplication {
 
   private List<SecurityReference> securityReferences() {
     AuthorizationScope authorizationScope =
-        new AuthorizationScope("allowAll", "allowAll");
+        new AuthorizationScope("allowAll", "Allow All");
     AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
     authorizationScopes[0] = authorizationScope;
     return List.of(new SecurityReference("JWT", authorizationScopes));
